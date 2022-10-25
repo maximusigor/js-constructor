3119555ecd465cedee32f76d92fa8c0adb939943
IhorShylov <shilov0209@gmail.com>
2022-10-25 15:59:56 +0300

    add js functional.

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', value: 'Hello World from JS!'},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'columns', value: [
+            '111111111',
+            '222222222',
+            '333333333',
+        ]},
+    {type: 'image', value: './assets/image.png'}
+]
